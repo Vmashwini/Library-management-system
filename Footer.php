034a7b0a94b2dd2ece2ec4e5070ecfae992415b6
Vmashwini <46575944+Vmashwini@users.noreply.github.com>
2019-04-05 20:10:18 +0100

    Add files via upload

---
+++ Footer.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+        <link href="librarystyling.css" rel="stylesheet" type="text/css"> 
+    </head>
+    <body>
+        
+       
+<!-- Footer -->
+
+<footer id="footer" class="site-footer" >
+		<div class="container">
+			<div class="row text-center text-xs-center text-sm-left text-md-left">
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5 style="color:white">Library</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href=""class="link-style"></i>Home</a></li>
+                                                <li><a href=""class="link-style"></i>Our Mission</a></li>
+						<li><a href=""class="link-style"></i>This Week's Reviews </a></li>
+						
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+                                    <h5 style="color:white ">Connect</h5>
+					<ul class="list-unstyled quick-links">
+                                            <li><a href="" class="link-style"></i>Become a Member</a></li>
+						<li><a href=""class="link-style"></i>Sign up for Newsletter</a></li>
+						<li><a href=""class="link-style"></i>Contact us</a></li>
+						
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5 style="color:white" >Social Media</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="" class="link-style"><i class="fab fa-facebook-square"></i>  Facebook</a></li>
+						<li><a href=""class="link-style"><i class="fab fa-twitter-square"></i>  Twitter</a></li>
+						<li><a href=""class="link-style"><i class="fab fa-instagram"></i>  Instagram</a></li>
+						
+					</ul>
+				</div>
+			</div>
+				
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					
+					<p class="h6">Copyright Women in Tech 2019. All rights reserved </p>
+				</div>
+				</hr>
+			</div>	
+		</div>
+	
+	<!-- ./Footer -->
+  </footer>
+ 
+        
+    </body>
+</html>
